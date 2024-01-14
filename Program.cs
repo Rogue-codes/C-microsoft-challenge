243c528e8c4a08c7c9d7605806b349cc3a613034
nnamdi <nnamdidanielosuji@gmail.com>
2024-01-14 03:08:44 +0100

    for each statement challenge

--- Program.cs
+++ Program.cs
@@ -1,27 +1,111 @@
-﻿Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
+﻿// initialize variables - graded assignments
+int examAssignments = 5;
 
-if (daysUntilExpiration == 0)
-{
-    Console.WriteLine("Your subscription has expired.");
-}
-else if (daysUntilExpiration == 1)
-{
-    Console.WriteLine("Your subscription expires within a day!");
-    discountPercentage = 20;
-}
-else if (daysUntilExpiration <= 5)
-{
-    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
-    discountPercentage = 10;
-}
-else if (daysUntilExpiration <= 10)
-{
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
-}
+int[] sophiaScores = [90, 86, 87, 98, 100, 94, 90];
+int[] andrewScores = [92, 89, 81, 96, 90, 89];
+int[] emmaScores = [90, 85, 87, 98, 68, 89, 89];
+int[] loganScores = [90, 95, 87, 88, 96, 96];
 
-if (discountPercentage > 0)
+// Student names
+string[] studentNames = ["Sophia", "Andrew", "Emma", "Logan"];
+
+int[] studentScores = new int[10];
+
+string currentStudentLetterGrade = "";
+
+// Display the Report Header
+Console.WriteLine("Student\t\tExam Score\tOverall Grade\tExtra Credit\n");
+
+
+foreach (string student in studentNames)
 {
-    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+    string currentStudent = student;
+    if (currentStudent == "Sophia")
+    {
+        studentScores = sophiaScores;
+    }
+    else if (currentStudent == "Andrew")
+    {
+        studentScores = andrewScores;
+
+    }
+    else if (currentStudent == "Emma")
+    {
+        studentScores = emmaScores;
+
+    }
+    else
+    {
+        studentScores = loganScores;
+    }
+
+    int sumAssignmentScores = 0;
+
+    decimal examScore = 0;
+    decimal extraCredits = 0;
+
+    // initialize/reset the calculated average of exam + extra credit scores
+    decimal currentStudentGrade = 0;
+
+    int gradedAssignments = 0;
+
+    foreach (int score in studentScores)
+    {
+        gradedAssignments += 1;
+        if (gradedAssignments <= examAssignments)
+        {
+            sumAssignmentScores += score;
+            examScore = (decimal)sumAssignmentScores/examAssignments;
+        }
+        else
+        {
+            // add the extra credit points to the sum - bonus points equal to 10% of an exam score
+            sumAssignmentScores += score / 10;
+        }
+
+    }
+    currentStudentGrade = (decimal)sumAssignmentScores / examAssignments;
+    extraCredits = currentStudentGrade - examScore;
+
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
+
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+
+    else
+        currentStudentLetterGrade = "F";
+
+    Console.WriteLine($"{student}\t\t{examScore}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}\t{examScore}({extraCredits} pts)");
+
 }
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2564d64a859410f6cb23a89e83b94b833a65f0c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
